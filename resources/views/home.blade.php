0729843a5639de6a75c384465b681fe4de21df7a
walterrodriguesjr <walterrjr.86@gmail.com>
2024-11-15 12:24:34 -0300

    atualizando

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,12 @@
 @extends('layouts.main_layout')
 
 @section('content')
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-6">
-                <form action="{{ route('submit') }}" method="post">
-                    @csrf
-                    <div class="mb-3">
-                        <label for="" class="form-label">Username:</label>
-                        <input type="text" name="username" class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <label for="" class="form-label">Password:</label>
-                        <input type="password" name="password" class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <button class="btn btn-primary">Login</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
+
+   @production
+       <h1>Estou em ambiente de produção</h1>
+       @else
+       <h1>Estou em ambiente de desenvolvimento</h1>
+
+   @endproduction
+
 @endsection
